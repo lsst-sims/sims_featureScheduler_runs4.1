ab565858d588a3b2d9f9f3d08e9941eb36abc2d6
Peter Yoachim <yoachim@uw.edu>
2024-10-15 16:49:02 -0700

    fpw

---
+++ technical/fp_weight/README.md
@@ -0,0 +1,1 @@
+Show how things break if you crank up the footprint weight
---
+++ technical/fp_weight/fpw.sh
@@ -0,0 +1,7 @@
+python fpw.py --fpw 0.1
+python fpw.py --fpw 0.6
+python fpw.py --fpw 1.5
+python fpw.py --fpw 5
+python fpw.py --fpw 20
+python fpw.py --fpw 100
+python fpw.py --fpw 500
