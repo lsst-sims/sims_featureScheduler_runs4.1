54faf5e0c3d5898222de4c112f08db6dbce29bc9
Peter Yoachim <yoachim@uw.edu>
2024-10-31 14:49:34 -0700

    sleeps

--- y1dt/selfcal.sh
+++ y1dt/selfcal.sh
@@ -1,10 +1,10 @@
-run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter g
-run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter r
-run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter i
-run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter z
-run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter y
-run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter g
-run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter r
-run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter i
-run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter z
-run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter y
+sleep 1 ; run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter g
+sleep 5 ; run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter r
+sleep 10 ; run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter i
+sleep 15 ; run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter z
+sleep 20 ; run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter y
+sleep 25 ; run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter g
+sleep 30 ; run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter r
+sleep 35 ; run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter i
+sleep 40 ; run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter z
+sleep 45 ; run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter y
