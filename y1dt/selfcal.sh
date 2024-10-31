fd7afb3dd285e6da8e20c2344113dc40e6c4a38e
Peter Yoachim <yoachim@uw.edu>
2024-10-31 15:56:41 -0700

    no clobber

--- y1dt/selfcal.sh
+++ y1dt/selfcal.sh
@@ -1,10 +1,10 @@
-sleep 1 ; run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter g
-sleep 5 ; run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter r
-sleep 10 ; run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter i
-sleep 15 ; run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter z
-sleep 20 ; run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter y
-sleep 25 ; run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter g
-sleep 30 ; run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter r
-sleep 35 ; run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter i
-sleep 40 ; run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter z
-sleep 45 ; run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter y
+run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter g --no_clobber
+run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter r --no_clobber
+run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter i --no_clobber
+run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter z --no_clobber
+run_selfcal_metric --db y1dt_dts0.5_v4.1_10yrs.db --filter y --no_clobber
+run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter g --no_clobber
+run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter r --no_clobber
+run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter i --no_clobber
+run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter z --no_clobber
+run_selfcal_metric --db y1dt_dts5.0_v4.1_10yrs.db --filter y --no_clobber
